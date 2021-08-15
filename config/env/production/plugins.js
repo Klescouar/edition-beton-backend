2895656582130b87d4fb346170f9978bc77bc8fc
Le Scouarnec <lescouarneckevin@protonmail.com>
2021-08-14 23:06:52 -0400

    feat(cloudinary) Add cloudinary plugins to handle upload

---
+++ config/env/production/plugins.js
@@ -0,0 +1,25 @@
+module.exports = ({ env }) => ({
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    introspection: true,
+    apolloServer: {
+      tracing: false,
+    },
+  },
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+});
